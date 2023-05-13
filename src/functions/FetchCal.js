4ea3ec901c719cb80b995e4faf599325cb6098ab
Derek <derek@frisbeeworld.com>
2023-05-13 18:22:46 +1000

    Testing with Cougars

--- src/functions/FetchCal.js
+++ src/functions/FetchCal.js
@@ -8,8 +8,14 @@ app.http('FetchCal', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
+		// Chargers U16s Joaquin
 		let scheduleUrl = 'https://www.playhq.com/basketball-victoria/org/carnegie-basketball-club/e064387a/junior-domestic-u8-u20-winter-2023/teams/carnegie-chargers-u16-boys-joaquin/9cbd6eaf';
 
+		// Bears U14 Waters
+		scheduleUrl = 'https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2023/teams/caulfield-bears-u14-boys-waters/c383bd05';
+
+		// McKinnon Cougars U16.4
+		scheduleUrl = 'https://www.playhq.com/basketball-victoria/org/mckinnon-cougars-basketball-club/5f84fbd9/victorian-junior-basketball-league-2023/teams/mckinnon-u16-boys-4/ae7ea468';
 
 		if (request.query.get('url'))
 		{
