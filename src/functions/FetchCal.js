5bb849ceeddda03b3ae18d2c4bedf69e088710bf
Derek <derek@frisbeeworld.com>
2024-04-05 21:33:41 +1100

    Add Bears U15

--- src/functions/FetchCal.js
+++ src/functions/FetchCal.js
@@ -19,7 +19,8 @@ app.http('FetchCal', {
 			"angus_u18" : "https://www.playhq.com/basketball-victoria/org/carnegie-basketball-club/e064387a/junior-domestic-u8-u20-summer-202324/teams/carnegie-chargers-u18-boys-joaquin/e025f609",
  			"angus_rep"  : "https://www.playhq.com/basketball-victoria/org/mckinnon-cougars-basketball-club/5f84fbd9/victorian-junior-basketball-league-2024/teams/mckinnon-u16-boys-1/3bcae747",
 			"angus_tour" : "https://www.playhq.com/basketball-victoria/org/southern-peninsula-tournament/iathletic-southern-peninsula-tournament-2023/teams/mckinnon-u16-boys-1/0f42d65c",
-			"jacko_u16" : "https://www.playhq.com/basketball-victoria/org/carnegie-basketball-club/e064387a/junior-domestic-u8-u20-summer-202324/teams/carnegie-chargers-u16-boys-owen/e829b923"
+			"jacko_u16" : "https://www.playhq.com/basketball-victoria/org/carnegie-basketball-club/e064387a/junior-domestic-u8-u20-summer-202324/teams/carnegie-chargers-u16-boys-owen/e829b923",
+			"bears_u15" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u15-boys-bridges/3a3aee69"
 		}
 
 		if (scheduleUrlMap[teamParam] === undefined)
