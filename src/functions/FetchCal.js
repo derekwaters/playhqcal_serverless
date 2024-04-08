041202ec8d53d4d9a712e09b5411e6d75f894025
Derek <derek@frisbeeworld.com>
2024-04-08 22:30:45 +1000

    Add all Bears teams

--- src/functions/FetchCal.js
+++ src/functions/FetchCal.js
@@ -20,7 +20,20 @@ app.http('FetchCal', {
  			"angus_rep"  : "https://www.playhq.com/basketball-victoria/org/mckinnon-cougars-basketball-club/5f84fbd9/victorian-junior-basketball-league-2024/teams/mckinnon-u16-boys-1/3bcae747",
 			"angus_tour" : "https://www.playhq.com/basketball-victoria/org/southern-peninsula-tournament/iathletic-southern-peninsula-tournament-2023/teams/mckinnon-u16-boys-1/0f42d65c",
 			"jacko_u16" : "https://www.playhq.com/basketball-victoria/org/carnegie-basketball-club/e064387a/junior-domestic-u8-u20-summer-202324/teams/carnegie-chargers-u16-boys-owen/e829b923",
-			"bears_u15" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u15-boys-bridges/3a3aee69"
+			"bears_u15" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u15-boys-bridges/3a3aee69",
+
+			"bears_u8m" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u8-mixed-rayner/c43ecc99",
+			"bears_u9m" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u9-mixed-dunbar/342afe09",
+			"bears_u11g" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-st-pauls-doggies-u11-girls-clohesy/d847f913",
+			"bears_u11m" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u11-mixed-ratner/0b9dd9e1",
+			"bears_u13m_fryar" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u13-mixed-fryar/4878f6f1",
+			"bears_u13m_colwell" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u13-mixed-colwell/35347692",
+			"bears_u14m" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u14-mixed-acklom/7c0081d1",
+			"bears_u15m_chrystie" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u15-boys-chrystie/8b24e6e1",
+			"bears_u16b_segar" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-kangaroos-u16-boys-segar-bernier/6da648b7",
+			"bears_u16b_wigney" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-kangaroos-u16-boys-wigney/84c089ee",
+			"bears_u17b" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u18-boys-rowlands/a9d2a6b9",
+			"bears_u18g" : "https://www.playhq.com/afl/org/caulfield-bears-junior-football-club-smjfl/4a2b554a/south-metro-junior-football-league-smjfl-2024/teams/caulfield-bears-u18-girls-marin/da865616"
 		}
 
 		if (scheduleUrlMap[teamParam] === undefined)
